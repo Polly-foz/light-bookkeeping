91293139cb357663caf401c163fdf951799f5245
polly-foz <polly.foz@gmail.com>
2020-02-29 11:58:22 +0800

    add hint: open on mobile phone

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
   </div>
 </template>
 
-
 <style lang="scss">
   @import "~@/assets/style/reset.scss";
   @import "~@/assets/style/helper.scss";
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import Nav from '@/components/Nav.vue'
-import LayoutWithNav from '@/components/LayoutWithNav.vue'
-import Icon from '@/components/Icon.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import Nav from '@/components/Nav.vue';
+import LayoutWithNav from '@/components/LayoutWithNav.vue';
+import Icon from '@/components/Icon.vue';
 import LayoutWithBackAndTitle from '@/components/LayoutWithBackAndTitle.vue';
 import DialogWithCover from '@/components/DialogWithCover.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.component('Nav',Nav)
-Vue.component('LayoutWithNav',LayoutWithNav)
-Vue.component('Icon',Icon)
-Vue.component('LayoutWithBackAndTitle',LayoutWithBackAndTitle)
-Vue.component("DialogWithCover",DialogWithCover)
+Vue.component('Nav', Nav);
+Vue.component('LayoutWithNav', LayoutWithNav);
+Vue.component('Icon', Icon);
+Vue.component('LayoutWithBackAndTitle', LayoutWithBackAndTitle);
+Vue.component('DialogWithCover', DialogWithCover);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
+
+if (document.documentElement.clientHeight > 540) {
+    window.alert('建议使用手机打开本页面，以保证浏览效果');
+}
